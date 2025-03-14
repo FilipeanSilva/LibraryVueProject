f03de210098a8ef86d71bfb27747fa60466ae5d3
Filipe Silva <fanssilva06@gmail.com>
2025-03-14 20:41:38 +0100

    fix: config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/LibraryVueProject/',  // <-- Change this to match your repo name
 })

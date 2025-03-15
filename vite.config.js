300582b4c9f961448a835a91b7e010ab7aad0b6b
Dr33-Dell <fanssilva06@gmail.com>
2025-03-15 10:55:05 +0100

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/LibraryVueProject/',  // This is critical for GitHub Pages
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
+    outDir: 'dist'
   }
 })
